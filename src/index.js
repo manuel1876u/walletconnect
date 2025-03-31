626fa9d7bda86421ebd9e0334f297831b94d8317
manuel1876u <manuelmanuel1876u@gmail.com>
2025-04-01 00:12:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Apppp />);
+root.render(<Apx />);
