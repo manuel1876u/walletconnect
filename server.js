41273f30f5a2acca0665857f7229287162b1c9aa
manuel1876u <manuelmanuel1876u@gmail.com>
2025-04-01 00:12:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@
  app.use(express.urlencoded({extended:false}));      
 
   // Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, "uild"))); 
+app.use(express.static(path.join(__dirname, "build"))); 
 
 // Handle all other routes and serve index.html
 app.get("*", (req, res) => {
